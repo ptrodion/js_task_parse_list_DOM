3804e61748562d97a07c28a455c20e20c0b0a7a2
ptrodion <rdnpetrenko@gmail.com>
2026-06-26 22:05:49 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
 'use strict';
 
-// write code here
+const elementParent = document.querySelector('ul');
+const elementsLi = elementParent.querySelectorAll('li');
+const elemListOfEmployees = Array.from(elementsLi);
+
+const parseSalary = (salaryStr) => {
+  if (!salaryStr) {
+    return 0;
+  }
+
+  return Number(salaryStr.replace(/[^0-9.]/g, '')) || 0;
+};
+
+const arrayListOfEmployees = elemListOfEmployees.map((employee) => {
+  const { position, salary, age } = employee.dataset;
+  const employeeName = employee.textContent.trim();
+
+  return {
+    name: employeeName,
+    position,
+    salary,
+    age,
+  };
+});
+
+const sortedBySalary = arrayListOfEmployees.sort(
+  (a, b) => parseSalary(b.salary) - parseSalary(a.salary),
+);
+
+const newListHTML = sortedBySalary
+  .map((user) => {
+    return `
+      <li
+      data-position="${user.position}"
+      data-salary="${user.salary}"
+      data-age="${user.age}"
+      >
+    ${user.name}
+    </li>
+    `;
+  })
+  .join('');
+
+elementParent.innerHTML = newListHTML;
